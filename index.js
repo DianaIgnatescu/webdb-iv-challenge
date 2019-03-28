7d7e4a22abbd02f3e5ba6269906f6f59b06b6d34
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-28 16:04:14 +0000

    STRETCH: Add more endpoints

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ server.get('/dishes', async (req, res) => {
   res.status(200).json(result);
 });
 
+server.get('/dishes/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await helpers.getDish(db, id);
+  res.status(200).json(result);
+});
+
 server.get('/recipes', async (req, res) => {
   const result = await helpers.getRecipes(db);
   res.status(200).json(result);
@@ -20,11 +26,14 @@ server.get('/recipes', async (req, res) => {
 server.post('/dishes', async (req, res) => {
   const dish = req.body;
   const result = await helpers.addDish(db, dish);
-        res.status(201).json(result)
+        res.status(201).json(result);
 });
 
-
-
+server.post('/recipes', async (req, res) => {
+  const recipe = req.body;
+  const result = await helpers.addRecipe(db, recipe);
+  res.status(201).json(result);
+});
 
 const port = 5000;
 server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
