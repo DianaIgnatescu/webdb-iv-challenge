c083a76d34c94f59e1413fcfe0c638af93e5e12b
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-28 15:41:04 +0000

    STRETCH: Create endpoints

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const knex = require('knex');
 const knexConfig = require('./knexfile').development;
+const helpers = require('./helpers');
 
 const db = knex(knexConfig);
-
 const server = express();
-
 server.use(express.json());
+
+server.get('/dishes', async (req, res) => {
+  const result = await helpers.getDishes(db);
+  res.status(200).json(result);
+});
+
+server.get('/recipes', async (req, res) => {
+  const result = await helpers.getRecipes(db);
+  res.status(200).json(result);
+});
+
+server.post('/dishes', async (req, res) => {
+  const dish = req.body;
+  const result = await helpers.addDish(db, dish);
+        res.status(201).json(result)
+});
+
+
+
 
 const port = 5000;
 server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
